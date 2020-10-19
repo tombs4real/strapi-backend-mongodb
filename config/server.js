69c4425221264ddfc3f18e9bab808226a7ec0767
James Tombs <james.tombs@utah.edu>
2020-10-18 23:15:14 -0600

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c4acc2d0820eacf3bc632ac2c3868660'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '473d706e-ec2b-48dd-aa44-af4f6f237231'
+};
